4399c512128054782407d25e393af1d01c5edcaf
Bradley M. Kuhn <bkuhn@ebb.org>
2015-09-02 18:07:29 +0000

    Rewrite script to use doveadm commands.
    I realized that dovecot now has a doveadm command, that allows
    for operations through dovecot on folders.  This seems a much better
    way to do spam training, so I've adapted this script to use those
    mechanisms.

--- etc/dovecot/scripts-available.d/daily-spam-train.sh
+++ etc/dovecot/scripts-available.d/daily-spam-train.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
+    
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 set -e
+
+if [ -z "$USER" ]; then
+    export USER=$LOGNAME
+fi
+
+if [ -z "$USER" ]; then
+    echo "Unable to determine which user is running this script.  Aborting!"
+    exit 1
+fi
+
+didAny=0
 
 for type in Ham Spam;
 do
     typeLower=`echo $type | perl -pe '$_ = "\l$_\E";'`
-    old=${HOME}/Mail/Old$type
-    for subdir in cur new; do
-       folder=${HOME}/Mail/TrainAs$type
-       if [ -d $folder/$subdir ]; then
-           echo "Training $type that you left in TrainAs$type ($subdir):  "
-           sa-learn --no-sync --$typeLower $folder/$subdir
-           if [ ! -d $old ]; then
-               mkdir -p $old/{cur,new,tmp}
+    finishedFolderName=Old$type
+    finishedFolder=${HOME}/Mail/$finishedFolderName
+    curFolderName=TrainAs$type
+    curFolder=${HOME}/Mail/$curFolderName
+    cnt=`doveadm mailbox status  -t messages ${curFolderName}|perl -pe 'die "failure to find message count in output of doveadmin mailbox status in \"$_\"" unless s/^\s*messages\s*=\s*(\d+)\s*$/$1/;'`
+    if [ $cnt -gt 0 ]; then
+        didAny=1
+        echo "Training $cnt message(s) of $typeLower that you left in ${curFolderName}:  "
+        for subdir in cur new; do
+            if [ -d $folder/$subdir ]; then
+                sa-learn --no-sync --$typeLower $curFolder/$subdir
             fi
-            cnt=`/bin/ls -1 $folder/$subdir| wc -l`
-            if [ $cnt -gt 0 ]; then
-               mv -f $folder/$subdir/* $old/cur/
-               rm -f $old/dovecot*
-               echo "Moved $cnt emails from TrainAs$type to the Old$type folder."
-            fi
-       fi
-    done
-    echo -e "\n"
+        done
+        # Next, the doveadm mailbox create command returns a
+        # non-zero $?  if the folder exists already.
+        doveadm mailbox create $finishedFolderName || true
+        # Next, the doveadm mailbox status command should exit with
+        # non-zero $?  if the mailbox doesn't exist at this point.
+        # So, that's used to test if the create worked.
+        doveadm mailbox status  -t messages $finishedFolderName > /dev/null
+        doveadm move $finishedFolder mailbox $curFolder ALL
+        echo "Moved $cnt emails from $curFolder to the $finishedFolder folder."
+        echo -e "\n"
+    fi
 done
 
-sa-learn --sync
-
-echo -e "\nYour spam database has been syncronized."
-
-echo "Training Complete."
+if [ $didAny -eq 1 ]; then
+    sa-learn --sync
+    echo -e "\nYour spam database has been syncronized."
+    echo "Training Complete."
+fi
